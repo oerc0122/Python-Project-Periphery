175d6784101f9388321e26a9811de75740ef9c9d
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2025-08-27 13:07:41 +0100

    Second revision

--- Resources/quadexm.py
+++ Resources/quadexm.py
@@ -1,19 +1,21 @@
 from __future__ import annotations
 
+from math import sqrt
+
 def quadratic(a: float, b: float, c: float) -> tuple[float, float]:
-     """
-     Solves the roots of a quadratic equation.
+    """
+    Solves the roots of a quadratic equation.
 
-     Uses the quadratic formula. Result must be real.
+    Uses the quadratic formula. Result must be real.
 
-     Parameters
-     ----------
-     a
-        :math:`x^2` coefficient.
-     b
-        :math:`x` coefficient.
-     c
-        Constant value.
+    Parameters
+    ----------
+    a
+       :math:`x^2` coefficient.
+    b
+       :math:`x` coefficient.
+    c
+       Constant value.
 
     Returns
     -------
@@ -35,8 +37,8 @@ def quadratic(a: float, b: float, c: float) -> tuple[float, float]:
 
     Examples
     --------
-    >>> quadratic(1., 0., 0.)
-    (0.0, -0.0)
+    >>> quadratic(1, 2, 0)
+    (0.0, -2.0)
     >>> quadratic(3., 0., -1.)
     (0.5773502691896257, -0.5773502691896257)
 
@@ -49,8 +51,8 @@ def quadratic(a: float, b: float, c: float) -> tuple[float, float]:
     .. [1] O. McNoleg, "The integration of GIS, remote sensing,
            expert systems ...
     """
-    
+
     det = b**2 - (4*a*c)
 
-    return ((-b + np.sqrt(det)) / (2*a),
-            (-b - np.sqrt(det)) / (2*a))    
+    return ((-b + sqrt(det)) / (2*a),
+            (-b - sqrt(det)) / (2*a))
